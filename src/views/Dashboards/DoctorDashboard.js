2ab3106aeefe272422e65c4cf9ee19ea0ca63beb
jimmylism <32603103+jimmylism@users.noreply.github.com>
2019-10-22 15:56:30 -0400

    temporary dashboard

--- src/views/Dashboards/DoctorDashboard.js
+++ src/views/Dashboards/DoctorDashboard.js
@@ -2,6 +2,10 @@ import React from 'react';
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Schedule from "@material-ui/icons/Schedule";
+import List from "@material-ui/icons/List";
 import { makeStyles } from "@material-ui/core/styles";
 // core components
 import Header from "components/Header/Header.js";
@@ -9,24 +13,28 @@ import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
 import Parallax from "components/Parallax/Parallax.js";
+import NavPills from "components/NavPills/NavPills.js";
 import RenderUser from "views/ProfilePage/RenderUser.js";
 
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
+import pillStyles from "assets/jss/material-kit-react/views/componentsSections/pillsStyle.js";
 
+const usePillStyles = makeStyles(pillStyles);
 const useStyles = makeStyles(styles);
 
 export default function ProfilePage(props) {
   const classes = useStyles();
+  const pillClasses = usePillStyles();
   const { ...rest } = props;
   return (
     <div>
       <Header
-        color="white"
+        color="transparent"
         brand="InfinityCare"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
-          height: 50,
+          height: 0,
           color: "white"
         }}
         {...rest}
@@ -37,9 +45,51 @@ export default function ProfilePage(props) {
           <div className={classes.container}>
             <GridContainer justify="center">
               <GridItem xs={12} sm={12} md={6}>
-                Doctor dashboard
+                <h2>Doctor dashboard</h2>
                 <RenderUser />
               </GridItem>
+              <GridItem xs={12} sm={12} md={8} lg={6}>
+              <NavPills
+                color="primary"
+                tabs={[
+                  {
+                    tabButton: "Dashboard",
+                    tabIcon: Dashboard,
+                    tabContent: (
+                      <span>
+                        <p>
+                          Dashboard for doctors and includes things tailored for the doctor userType
+                        </p>
+                        <br />
+                      </span>
+                    )
+                  },
+                  {
+                    tabButton: "Schedule",
+                    tabIcon: Schedule,
+                    tabContent: (
+                      <span>
+                        <p>
+                          Schedule for each day and the patient that books the doctor.
+                        </p>
+                        <br />
+                      </span>
+                    )
+                  },
+                  {
+                    tabButton: "Tasks",
+                    tabIcon: List,
+                    tabContent: (
+                      <span>
+                        <p>
+                          Tasks such as confirm appointments, cancel appointments, etc..
+                        </p>
+                      </span>
+                    )
+                  }
+                ]}
+              />
+            </GridItem>
             </GridContainer>
           </div>
         </div>
--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -12,11 +12,14 @@ import Parallax from "components/Parallax/Parallax.js";
 import RenderUser from "views/ProfilePage/RenderUser.js";
 
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
+import styles from "assets/jss/material-kit-react/views/componentsSections/pillsStyle.js";
 
+const usePillStyles = makeStyles(pillStyles);
 const useStyles = makeStyles(styles);
 
 export default function ProfilePage(props) {
   const classes = useStyles();
+  const pillClasses = usePillStyles();
   const { ...rest } = props;
   return (
     <div>
@@ -39,6 +42,93 @@ export default function ProfilePage(props) {
               <GridItem xs={12} sm={12} md={6}>
                 <RenderUser />
               </GridItem>
+              <GridItem xs={12} sm={12} md={8} lg={6}>
+              <NavPills
+                color="primary"
+                tabs={[
+                  {
+                    tabButton: "Dashboard",
+                    tabIcon: Dashboard,
+                    tabContent: (
+                      <span>
+                        <p>
+                          Collaboratively administrate empowered markets via
+                          plug-and-play networks. Dynamically procrastinate B2C
+                          users after installed base benefits.
+                        </p>
+                        <br />
+                        <p>
+                          Dramatically visualize customer directed convergence
+                          without revolutionary ROI. Collaboratively
+                          administrate empowered markets via plug-and-play
+                          networks. Dynamically procrastinate B2C users after
+                          installed base benefits.
+                        </p>
+                        <br />
+                        <p>
+                          Dramatically visualize customer directed convergence
+                          without revolutionary ROI. Collaboratively
+                          administrate empowered markets via plug-and-play
+                          networks. Dynamically procrastinate B2C users after
+                          installed base benefits.
+                        </p>
+                      </span>
+                    )
+                  },
+                  {
+                    tabButton: "Schedule",
+                    tabIcon: Schedule,
+                    tabContent: (
+                      <span>
+                        <p>
+                          Efficiently unleash cross-media information without
+                          cross-media value. Quickly maximize timely
+                          deliverables for real-time schemas.
+                        </p>
+                        <br />
+                        <p>
+                          Dramatically maintain clicks-and-mortar solutions
+                          without functional solutions. Dramatically visualize
+                          customer directed convergence without revolutionary
+                          ROI. Collaboratively administrate empowered markets
+                          via plug-and-play networks. Dynamically procrastinate
+                          B2C users after installed base benefits.
+                        </p>
+                      </span>
+                    )
+                  },
+                  {
+                    tabButton: "Tasks",
+                    tabIcon: List,
+                    tabContent: (
+                      <span>
+                        <p>
+                          Collaboratively administrate empowered markets via
+                          plug-and-play networks. Dynamically procrastinate B2C
+                          users after installed base benefits.
+                        </p>
+                        <br />
+                        <p>
+                          Dramatically visualize customer directed convergence
+                          without revolutionary ROI. Collaboratively
+                          administrate empowered markets via plug-and-play
+                          networks. Dynamically procrastinate B2C users after
+                          installed base benefits.
+                        </p>
+                        <br />
+                        <p>
+                          Dramatically visualize customer directed convergence
+                          without revolutionary ROI. Collaboratively
+                          administrate empowered markets via plug-and-play
+                          networks. Dynamically procrastinate B2C users after
+                          installed base benefits.
+                        </p>
+                      </span>
+                    )
+                  }
+                ]}
+              />
+            </GridItem>
             </GridContainer>
           </div>
         </div>
