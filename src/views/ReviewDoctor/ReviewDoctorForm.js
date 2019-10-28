4efee6d544e29159900d95e4e07ad3648d17694f
Jack Noble <jarnoble@indiana.edu>
2019-10-28 09:34:23 -0400

    fixed error

--- src/views/ReviewDoctor/ReviewDoctorForm.js
+++ src/views/ReviewDoctor/ReviewDoctorForm.js
@@ -11,10 +11,50 @@ import axios from "axios"
 
 // TODO - modify form fields to better suit form
 // TODO - links & backend
-//
+// TODO - delete & modify copy & pasted OTP code
 
 export default class ReviewDoctorForm extends React.Component {
-    handleSubmit = () => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            otp: "",
+            cardAnimaton: "cardHidden",
+        };
+        this.handleOTPChange = this.handleOTPChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleOTPChange = event => {
+        this.setState({ otp: event.target.value });
+    };
+
+    handleSubmit = event => {
+        // TODO: Why did Jack add this line?
+        //event.preventDefault();
+
+        const user = {
+            otp: this.state.otp,
+            isOtpAccurate: ""
+        };
+
+        var targetUrl = 'https://infinity-care.herokuapp.com/otp/' + window.localStorage.getItem("userType");
+        var queryString = "?otp=" + this.state.otp;
+
+        axios({
+            method : 'post',
+            url: targetUrl + queryString,
+            headers: {'Content-Type': 'application/json', Accept: 'application/json'},
+            data : {
+                username : this.state.username,
+                password : this.state.password
+            }
+        }).then(res => {
+            if(user.isOtpAccurate) {
+                this.setState({successful: true})
+            }
+        }).catch(() => {
+            this.setState({successful: true})
+        })
     };
 
     render() {
