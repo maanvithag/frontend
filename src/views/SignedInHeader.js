b4009d2e44b02055f2f149d550a99f935a87d1f6
jimmylism <32603103+jimmylism@users.noreply.github.com>
2019-10-30 17:28:41 -0400

    update signed in header with search and profile

--- src/views/SignedInHeader.js
+++ src/views/SignedInHeader.js
@@ -11,9 +11,12 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Tooltip from "@material-ui/core/Tooltip";
 import TypeSelect from "views/Modals/TypeSelect.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
 
 // @material-ui/icons
 import { Apps, CloudDownload } from "@material-ui/icons";
+import Search from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
 // core components
 import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
@@ -26,7 +29,42 @@ const useStyles = makeStyles(styles);
 export default function SignedInHeaders(props) {
   const classes = useStyles();
   return (
-    <List className={classes.list}>
+    <List className={classes.list} style={{display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+      <ListItem>
+      <div style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center'}}>
+        <CustomInput
+          primary
+          inputRootCustomClasses={classes.inputRootCustomClasses}
+          formControlProps={{
+            className: classes.formControl
+          }}
+          inputProps={{
+            placeholder: "Search",
+            inputProps: {
+              "aria-label": "Search",
+              className: classes.searchInput
+            }
+          }}
+        />
+        <Button justIcon round color="primary">
+          <Search className={classes.searchIcon} />
+        </Button>
+      </div>
+      </ListItem>
+      <ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+        /*
+          href="#pablo"
+          className={classes.navLink}
+          onClick={e => e.preventDefault()}
+          */
+          color="primary"
+        >
+          <AccountCircle className={classes.icons} /> Profile
+        </Button>
+      </ListItem>
+      </ListItem>
       <ListItem>
         <Link to="/" className={classes.link}>
           <Button color="primary">
