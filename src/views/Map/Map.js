95b6fce8581fb9c3525cc4381af4f1b8281e3ca9
jimmylism <32603103+jimmylism@users.noreply.github.com>
2019-11-03 17:13:01 -0500

    Dynamically render markers

--- src/views/Map/Map.js
+++ src/views/Map/Map.js
@@ -1,26 +1,42 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
-import Marker from "views/Map/Marker.js";
+import Marker from './Marker';
 
-const Map = (props) => {
+
+
+const SimpleMap = (props) => {
+  const getMapOptions = (maps) => {
+    return {
+      disableDefaultUI: true,
+      mapTypeControl: true,
+      streetViewControl: true,
+      styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
+    };
+  };
+
     const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
     const [zoom, setZoom] = useState(11);
+    const [location, setLocation] = useState([
+        {"doctor": "doctor1", "lat": "11.0168", "lng": "76.9558" }, 
+        {"doctor": "doctor2", "lat": "11.0200", "lng": "77.000" }
+    ]);
     return (
         <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyB8gxX2h4_2Xw1sKg-jDdv8T_uut8-KV8s' }}
           defaultCenter={center}
           defaultZoom={zoom}
+          options={getMapOptions}
         >
-          <Marker
-            lat={11.0168}
-            lng={76.9558}
-            name="Center marker"
+        { location.map((item, index) => (<Marker
+            lat={item.lat}
+            lng={item.lng}
+            name={item.doctor}
             color='blue'
-          />
+        />))}
         </GoogleMapReact>
       </div>
     );
 }
 
-export default Map;
+export default SimpleMap;
