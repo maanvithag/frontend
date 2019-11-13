ba0539b4c5cb78b44360de0e9119a163a91e45fe
jimmylism <32603103+jimmylism@users.noreply.github.com>
2019-11-13 02:51:29 -0500

    map

--- src/views/Map/Map.js
+++ src/views/Map/Map.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
+import Geocode from "react-geocode";
 
 const SimpleMap = (props) => {
   const getMapOptions = (maps) => {
@@ -11,12 +12,24 @@ const SimpleMap = (props) => {
       styles: [{ featureType: 'poi', elementType: 'labels', stylers: [{ visibility: 'on' }] }],
     };
   };
+  
+    Geocode.setApiKey("AIzaSyB8gxX2h4_2Xw1sKg-jDdv8T_uut8-KV8s");
+    Geocode.fromAddress("Eiffel Tower").then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+        setCenter(lat, lng);
+      },
+      error => {
+        console.error(error);
+      }
+    );
 
-    const [center, setCenter] = useState({lat: 11.0168, lng: 76.9558 });
+    const [center, setCenter] = useState({lat: 39.1653, lng: -86.5264 });
     const [zoom, setZoom] = useState(11);
-    const [location, setLocation] = useState([
-        {"doctor": "doctor1", "lat": "11.0168", "lng": "76.9558" }, 
-        {"doctor": "doctor2", "lat": "11.0200", "lng": "77.000" }
+    const [marker, setMarker] = useState([
+        {"doctor": "doctor1", "lat": "39.1853", "lng": "-86.5464" }, 
+        {"doctor": "doctor2", "lat": "39.1453", "lng": "-86.5064" }
     ]);
     return (
         <div style={{ height: '50vh', width: '100%' }}>
@@ -26,7 +39,7 @@ const SimpleMap = (props) => {
           defaultZoom={zoom}
           options={getMapOptions}
         >
-        { location.map((item, index) => (<Marker
+        { marker.map((item, index) => (<Marker
             lat={item.lat}
             lng={item.lng}
             name={item.doctor}
